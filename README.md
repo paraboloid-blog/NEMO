a4319fb1feb204e75887605a164da5a2e11ff2df
Reinhard Winkler <reinhard.winkler@protonmail.ch>
2019-07-30 15:43:31 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SCP Cloud Foundry - Playing around with Node.js, MongoDB and OpenUI5
+
+Details can be found in the [SAP Community](https://blogs.sap.com/2016/09/05/hcp-cloud-foundry-playing-around-with-node-mongodb-and-ui5/).
---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let express = require('express');
+let bodyParser = require('body-parser');
+let cfenv = require('cfenv');
+
+// create express instance
+let oApp = express();
+
+// Cloud Foundry environment variables
+let oAppEnv = cfenv.getAppEnv();
+
+// body parser middleware to handle URL parameter and JSON bodies
+oApp.use(bodyParser.urlencoded({extended: false}));
+oApp.use(bodyParser.json());
+
+// "static" resources 
+oApp.use(express.static(__dirname + '/client/public/webapp'));  
+
+// connect to mongodb
+require('./server/db/mongo-connect.js')(oAppEnv);
+
+// client express routes
+require('./client/routes/routes.js')(oApp);
+
+// api
+require('./server/api/info/info.js')(oApp, oAppEnv);
+require('./server/api/users/users.js')(oApp);
+
+// express app listener
+oApp.listen(oAppEnv.port, function(){
+    console.log('Server listening at ' + oAppEnv.url);
+});
+
